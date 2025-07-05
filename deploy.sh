f8bfb9f1e83ae6319f3b9f596dcc9a78a6229e28
odonML <odon_ml@outlook.com>
2025-07-05 06:39:49 -0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
+
+BRANCH=$1
+
+if("$BRANCH" == "main"); then
+  DEPLOY_PATH="/home/azureuser/github-action-page-azure"
+else
+  DEPLOY_PATH="/home/azureuser/stage"
+fi
 ssh -o StrictHostKeyChecking=no azureuser@20.84.42.99 <<HTML
 
-  cd /home/azureuser/github-action-page-azure
-  git pull --rebase origin main
-  
+  cd $DEPLOY_PATH
+  git pull --rebase origin $BRANCH
+
 
 HTML
